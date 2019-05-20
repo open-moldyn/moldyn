a61f731c31b4439bf65415ecf859123696462366
ArthurLuciani <arthur.luciani@ens-rennes.fr>
2019-05-20 14:51:12 +0200

    First tests and corrections

--- moldyn/utils/data_mng.py
+++ moldyn/utils/data_mng.py
@@ -11,16 +11,22 @@ class ParamIO(dict):
         self.file_name = file
 
     def __enter__(self):
-        with open(self.file_name, mode='r') as file:
-            params = json.load(self.file_name,
-                               parse_float=True, parse_int=True)
-        for key, value in params.items():
-            self[key] = value
+        try:
+            with open(self.file_name, mode='r') as file:
+                params = json.load(self.file_name,
+                                   parse_float=True, parse_int=True)
+
+            for key, value in params.items():
+                self[key] = value
+
+        except FileNotFoundError:
+            print("File does not YET exists")
+            pass
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         with open(self.file_name, mode='w') as file:
-            json.dump(self.params, ensure_ascii=False, indent=4)
+            json.dump(dict(self), file, ensure_ascii=False, indent=4)
 
     def fromDict(self, rdict):
         for key, value in rdict.items():
@@ -28,7 +34,7 @@ class ParamIO(dict):
 
 
 class DynStateIO:
-    def __init__(self, dynState, file : str, mode):
+    def __init__(self, dynState, file : Leaf, mode):
         self.dynState = dynState
         self.mode = mode
         self.file_name = file
@@ -40,14 +46,13 @@ class DynStateIO:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.file.close()
 
-    def save(self, *args, **kwargs):
+    def save(self, arr, **kwargs):
         assert self.mode != 'r'
-        if self.file_name.endswith(".npy"):
-            np.save(self.file, args)
+        np.save(self.file, arr)
 
-    def load(self, N):
-        assert self.mode == 'r'
-        return np.load(self.file)
+    def load(self):
+        assert 'r' in self.mode
+        return np.load(self.file, allow_pickle=True)
 
 
 class DynState(Treant):
@@ -55,13 +60,25 @@ class DynState(Treant):
     VEL = "velocities.npy"
     PAR = "parameters.json"
 
-    def __init__(self, dirpath, treant):
+    def __init__(self, treant):
         super().__init__(treant)
 
     def open(self, file, mode='r'):
-        if mode=='r':
-            assert file in self.leaves()
         if file.endswith(".npy"):
+            if not(mode.endswith("+b")):
+                mode += "+b"
             return DynStateIO(self, self.leafloc[file], mode)
         elif file.endswith(".json"):
             return ParamIO(self, self.leafloc[file])
+
+
+t = 0
+def test(dirpath):
+    global t
+    t = DynState(dirpath)
+    with t.open(t.POS, 'w') as IO:
+        IO.save(np.ones(5))
+
+    with t.open(t.POS, 'r') as IO:
+        print(IO.load())
+
