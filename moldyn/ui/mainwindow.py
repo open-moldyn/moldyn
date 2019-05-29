0b6304855e85b30be23023fd6634a09f2474e109
Alexandre <alexandre.faye-bedrin@ens-rennes.fr>
2019-05-29 17:27:28 +0200

    Travail sur l'UI de construction de modèle

--- moldyn/ui/mainwindow.py
+++ moldyn/ui/mainwindow.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import QThread, pyqtSignal
 
 from .qt.mainwindow import Ui_MainWindow
 
@@ -8,6 +9,7 @@ from .model_viewer import ModelView
 from ..simulation.builder import Model
 from ..simulation.runner import Simulation
 
+
 class MoldynMainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -15,13 +17,31 @@ class MoldynMainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        # Panneau modèle
         self.ui.newModelBtn.clicked.connect(self.create_model)
 
         self.ui.gotoSimuBtn.clicked.connect(self.goto_simu)
 
-        self.ui.tab_simu.setEnabled(False)
+        # Panneau simu
+
+        self.ui.iterationsSpinBox.valueChanged.connect(self.update_simu_time)
+        self.ui.simulationTimeLineEdit.editingFinished.connect(self.update_iters)
+
+        self.ui.simuBtn.clicked.connect(self.simulate)
 
         self.show()
+
+    def update_iters(self):
+        try:
+            t = float(self.ui.simulationTimeLineEdit.text())
+        except:
+            self.update_simu_time()
+        else:
+            self.ui.iterationsSpinBox.setValue(int(t/self.simulation.model.dt))
+
+    def update_simu_time(self, v=None):
+        self.ui.simulationTimeLineEdit.setText(str(self.model.dt*self.ui.iterationsSpinBox.value()))
+        self.ui.simuProgressBar.setMaximum(self.ui.iterationsSpinBox.value())
 
     def set_model(self, model):
         self.model = model
@@ -30,9 +50,27 @@ class MoldynMainWindow(QMainWindow):
         self.simulation = Simulation(self.model)
         self.model_view = ModelView(self.simulation.model)
 
+        self.update_simu_time()
+
+        self.ui.RTViewBtn.clicked.connect(self.model_view.show)
+
     def create_model(self):
         self.cmd = CreateModelDialog(self)
         self.cmd.show()
 
     def goto_simu(self):
         self.ui.tabWidget.setCurrentWidget(self.ui.tab_simu)
+
+    def simulate(self): # NE FONCTIONNE PAS
+        self.ui.simuBtn.setEnabled(False)
+        updated_signal = pyqtSignal(int)
+        def run():
+            self.simulation = Simulation(self.model)
+            self.model_view = ModelView(self.simulation.model)
+            def up(s):
+                updated_signal.emit(s.current_iter)
+            self.simulation.iter(self.ui.iterationsSpinBox.value(), up)
+            self.ui.simuBtn.setEnabled(True)
+        self.simu_thr = QThread()
+        self.simu_thr.run = run
+        self.simu_thr.start()
