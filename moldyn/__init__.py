f2b24a0b1f2512df19c07e1a0d89607bf85a5398
ArthurLuciani <arthur.luciani@ens-rennes.fr>
2019-06-18 14:41:27 +0200

    publishing

--- moldyn/__init__.py
+++ moldyn/__init__.py
@@ -5,3 +5,15 @@ from . import simulation
 
 __all__ = ["processing", "simulation", "utils"]
 __version = "0.0.1"
+
+def gui():
+    import sys
+    from PyQt5.QtWidgets import QApplication
+    from moldyn.ui.mainwindow import MoldynMainWindow
+
+    app = QApplication(sys.argv)
+
+    window = MoldynMainWindow()
+    window.show()
+
+    app.exec_()
