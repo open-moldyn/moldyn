strain CPU works but wrong results

moldyn/processing/data_proc.py
@@ -1,5 +1,6 @@
 import os
 from functools import wraps
+from pprint import pprint
 
 import numpy as np
 import numexpr as ne
@@ -7,6 +8,7 @@ from matplotlib.tri import TriAnalyzer, Triangulation, UniformTriRefiner
 from scipy.spatial import Voronoi, ConvexHull
 import moderngl
 
+from moldyn.processing.strain_CPU import StrainComputeCPU
 from moldyn.simulation.builder import Model
 from moldyn.utils import gl_util
 
@@ -245,16 +247,32 @@ class StrainComputeGPU:
         return np.frombuffer(self._BUFFER_E.read(), dtype=np.float32).reshape(self.array_shape)
 
 
-@cached
-def compute_strain(model0:Model, model1:Model, rcut):
+
+def compute_strain_GPU(model0:Model, model1:Model, rcut):
     params = model0.params.copy()
     params["RCUT"] = rcut
     strain_compute = StrainComputeGPU(params)
     strain_compute.set_post(model0.pos)
     strain_compute.set_posdt(model1.pos)
     strain_compute.compute()
+    return strain_compute.get_eps()
+
+def compute_strain_CPU(model0:Model, model1:Model, rcut):
+    params = model0.params.copy()
+    params["RCUT"] = rcut
+    strain_compute = StrainComputeCPU(params)
+    strain_compute.set_post(model0.pos)
+    strain_compute.set_posdt(model1.pos)
+    strain_compute.compute()
+    return strain_compute.get_eps()
+
+@cached
+def compute_strain(model0:Model, model1:Model, rcut):
     try:
-        return strain_compute.get_eps()
+        eps = compute_strain_CPU(model0, model1, rcut)
+        eps2 = compute_strain_GPU(model0, model1, rcut)
+        pprint(eps[:10]-eps2[:10])
+        return eps-eps2
     except Exception as e:
-        print(repr(e))
         raise
+        return compute_strain_GPU(model0, model1, rcut)

moldyn/processing/strain_CPU.py
@@ -1,9 +1,11 @@
 """
-Forces calculator.
+Strain calculator.
 Runs on CPU.
 """
+import weakref
+from pprint import pprint
+
 from numpy.linalg import inv
-from numpy.matlib import eye
 
 """
 installer icc-rt et tbb sur les machines à processeur intel
@@ -19,12 +21,9 @@ import ctypes
 
 @numba.njit(nogil=True, cache=True)
 def _iterate(current_pos, x, pos, posdt, NPART, rcut, LENGTH_X, LENGTH_Y, SHIFT_X, SHIFT_Y, X_PERIODIC, Y_PERIODIC):
-    f = np.zeros((2,))
-    e = 0.0
-    m = 0.0
-    X = np.matrix(np.zeros((2,2)))
-    Y = np.matrix(np.zeros((2,2)))
-    eps = np.matrix(np.zeros((2,2)))
+    X = np.zeros((2,2))
+    Y = np.zeros((2,2))
+    eps = np.zeros((2,2))
     for n in range(NPART):
         if x==n:
             continue
@@ -63,7 +62,7 @@ def _iterate(current_pos, x, pos, posdt, NPART, rcut, LENGTH_X, LENGTH_Y, SHIFT_
                         X[i,j] += distxy[i]*distxydt[j]
                         Y[i,j] += distxydt[i] * distxydt[j]
 
-    eps = X*inv(Y.T) - eye(2)
+    eps[:] = X*inv(Y.transpose()) - np.eye(2)
     return eps
 
 
@@ -78,6 +77,7 @@ def _par_iterate(current_pos, i, RCUT, NPART, LENGTH_X, LENGTH_Y, X_PERIODIC, Y_
 
 def _p_compute_strain(pool, _pos, _posdt, eps_arr, RCUT, NPART, LENGTH_X, LENGTH_Y, X_PERIODIC, Y_PERIODIC, SHIFT_X, SHIFT_Y,
                       offset, end):
+
     pos = np.array(_pos).reshape(NPART,2)
 
     gen = ((pos[i,:], i, RCUT, NPART, LENGTH_X, LENGTH_Y, X_PERIODIC, Y_PERIODIC, SHIFT_X, SHIFT_Y)
@@ -85,7 +85,7 @@ def _p_compute_strain(pool, _pos, _posdt, eps_arr, RCUT, NPART, LENGTH_X, LENGTH
 
     sortie = np.array(pool.starmap(_par_iterate, gen))
 
-    eps_arr[:, :, :] = sortie.copy()
+    eps_arr[:] = sortie[:]
 
 _pos_array = None
 _posdt_array = None
@@ -100,12 +100,14 @@ class StrainComputeCPU:
 
     def __init__(self, consts, compute_npart=None, compute_offset=0):
 
-        self.consts = consts
+        self.consts = dict()
+        for key, item in consts.items():
+            self.consts[key.upper()] = item
 
         self.compute_offset = max(0, compute_offset)
 
-        self.npart = consts["NPART"]
-        self.compute_npart = compute_npart or (consts["NPART"] - self.compute_offset)
+        self.npart = self.consts["NPART"]
+        self.compute_npart = compute_npart or (self.consts["NPART"] - self.compute_offset)
 
         self.compute_npart = min(self.compute_npart, self.npart)
 
@@ -122,7 +124,6 @@ class StrainComputeCPU:
 
     def _compute_strain(self):
         RCUT = self.consts["RCUT"]
-        N_A = self.consts["N_A"]
         NPART = self.consts["NPART"]
         LENGTH_X = self.consts["LENGTH_X"]
         LENGTH_Y = self.consts["LENGTH_Y"]
@@ -132,8 +133,8 @@ class StrainComputeCPU:
         SHIFT_X = LENGTH_X / 2
         SHIFT_Y = LENGTH_Y / 2
         end = NPART
-        _p_compute_strain(self._pool, self._POS, self._POSDT, self._F, self._PE, RCUT, N_A, NPART, LENGTH_X, LENGTH_Y, X_PERIODIC,
-                          Y_PERIODIC, SHIFT_X, SHIFT_Y)
+        _p_compute_strain(self._pool, self._POS, self._POSDT, self._EPS, RCUT, NPART, LENGTH_X, LENGTH_Y, X_PERIODIC,
+                          Y_PERIODIC, SHIFT_X, SHIFT_Y, self.compute_offset, end)
 
     def _join_thr(self):
         if self._thr_run:
@@ -146,10 +147,11 @@ class StrainComputeCPU:
     def set_posdt(self, pos):
         self._POSDT[:] = pos.reshape((self.npart * 2,))
 
-    def compute(self):
-        self._thread = threading.Thread(target=self._compute_strain)
-        self._thread.start()
-
     def get_eps(self):
         self._join_thr()
         return self._EPS[:]
+
+    def compute(self):
+        self._thr_run = True
+        self._thread = threading.Thread(target=self._compute_strain)
+        self._thread.start()

moldyn/processing/visualisation.py
@@ -232,7 +232,7 @@ def deformation_volume(model0, model1, rcut, levels=50):
     cmap = plt.get_cmap("plasma")
     dV = eps.trace(axis1=1, axis2=2)
     if type(levels) == int:
-        levels = np.linspace(min(dV), max(dV), levels)
+        levels = np.linspace(min(dV), max(dV)+1e-30, levels)
     CS = plt.tricontourf(*model1.pos.T, dV, levels=levels, cmap=cmap)
     cbar = fig.colorbar(CS)
     cbar.ax.set_ylabel('local compression')
@@ -244,7 +244,7 @@ def deformation_dev(model0, model1, rcut, levels=50):
     cmap = plt.get_cmap("plasma")
     dV = eps[:,0,1]
     if type(levels) == int:
-        levels = np.linspace(min(dV), max(dV), levels)
+        levels = np.linspace(min(dV), max(dV)+1e-30, levels)
     CS = plt.tricontourf(*model1.pos.T, dV, levels=levels, cmap=cmap)
     cbar = fig.colorbar(CS)
     cbar.ax.set_ylabel('local shear')