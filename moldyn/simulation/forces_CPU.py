46c6aa24ec16fd5a86ab0ff97e12902cb9a7bf24
Alexandre <alexandre.faye-bedrin@ens-rennes.fr>
2019-07-09 15:02:41 +0200

    atomes supplémentaires (yay) et doc

--- moldyn/simulation/forces_CPU.py
+++ moldyn/simulation/forces_CPU.py
@@ -1,6 +1,7 @@
 """
 Forces calculator.
-Runs on CPU.
+Runs on CPU, using `numba` for JIT and `multiprocessing` for multicore.
+See `ForcesComputeGPU` for documentation.
 """
 
 """
--- moldyn/simulation/forces_GPU.py
+++ moldyn/simulation/forces_GPU.py
@@ -67,6 +67,7 @@ class ForcesComputeGPU:
 
     def set_pos(self, pos):
         """
+        Set position array and launches forces computation.
 
         Parameters
         ----------
