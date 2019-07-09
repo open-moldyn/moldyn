408369733808298b72bd766841c6a015ca53d2fe
Alexandre <alexandre.faye-bedrin@ens-rennes.fr>
2019-07-09 15:21:13 +0200

    atomes supplémentaires (yay) et doc

--- moldyn/data/atom_preset.py
+++ moldyn/data/atom_preset.py
@@ -6,7 +6,16 @@ Atom = (
 )
 """
 
+kB = 1.38064852e-23 # Boltzmann constant
+NA = 6.0221409e+23 # Avogadro number
+m_nucleon = 1.674e-27 # mass of a nucleon
+
 atoms = {
+    "Lithium": (
+        0.690/NA,
+        1.51e-10,
+        7*m_nucleon,
+    ),
     "Argon": (
         1.65e-21,
         3.4e-10,
@@ -26,5 +35,5 @@ atoms = {
         1.63e-21,
         3.46e-10,
         2.66e-26
-    )
+    ),
 }
